89aed8d9e418d3aa7f41c5f34d49897e85d1fd87
Erika <erikasalcedo@Erikas-MacBook-Pro.local>
2021-04-28 22:36:57 -0700

    added modal component with conditional rendering

--- src/components/Nav/__test__/index.test.js
+++ src/components/Nav/__test__/index.test.js
@@ -8,9 +8,10 @@ import Nav from '..';
 const categories = [
     { name: 'portraits', description: 'Portraits of people in my life' }
   ]
-  const mockCurrentCategory = jest.fn();
-  const mockSetCurrentCategory = jest.fn();
-
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 
 
 //configure test environment
@@ -19,9 +20,11 @@ afterEach(cleanup);
 
 it('renders', () => {
     render(<Nav
-      categories={categories}
-      setCurrentCategory={mockSetCurrentCategory}
-      currentCategory={mockCurrentCategory}
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+        contactSelected={mockContactSelected}
+        setContactSelected={mockSetContactSelected}
     />);
   })
   
--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
+import Modal from '../Modal'; //modal will be child component of PhotoList
 
 const PhotoList = ({category}) => {
+  const [currentPhoto, setCurrentPhoto] = useState();
+  //initial state of isModalOpsn set to false because we dont want modal to open
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
 
     const [photos] = useState([
         {
@@ -102,14 +107,21 @@ const PhotoList = ({category}) => {
 //go through ea photo in photos array and match category
 const currentPhotos = photos.filter((photo) => photo.category === category);
 
+//define toggleModal
+const toggleModal = (image, i) => {
+  setCurrentPhoto({...image, index: i});
+  setIsModalOpen(!isModalOpen);
+}
   return (
     <div>
+      {isModalOpen && <Modal onClose={toggleModal} currentPhoto={currentPhoto} />}
     <div className="flex-row">
       {currentPhotos.map((image, i) => (
         <img
           src={require(`../../assets/small/${category}/${i}.jpg`)}
           alt={image.name}
           className="img-thumbnail mx-1"
+          onClick={() => toggleModal(image, i)}
           key={image.name}
         />
       ))}
